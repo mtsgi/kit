fe3df98624e659951dee83bd8df2d44ee7b4727c
mtsgi <oq@live.jp>
2019-08-10 18:47:45 +0900

    Add Data Observing System by "kit-observe" Attribute [0.2.0]

--- app/dialog/dialog.js
+++ app/dialog/dialog.js
@@ -1,16 +1,6 @@
-app_dialog(pid);
-
-function app_dialog(_pid) {
-    S.dom(_pid).css("min-width", "400px").css("text-align", "center");
-    if( System.args[pid] ){
-        $("#winc" + _pid + " .dialog-title").text(System.args[_pid].title);
-        $("#winc" + _pid + " .dialog-content").html(System.args[_pid].content);
-    }
-    $("#winc" + _pid + " .dialog-cancel").on("click", function(){
-        System.close(_pid);
-    });
-    $("#winc" + _pid + " .dialog-ok").on("click", function(){
+((_pid) => {
+    App.event( _pid, "ok", ()=>{
         System.args[_pid].func();
         System.close(_pid);
-    });
-}
+    } );
+})(pid);
