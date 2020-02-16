cb7a263764637a9bb9881e12028dfd7a740b1300
mtsgi <oq@live.jp>
2020-02-17 06:07:55 +0900

    Update app/screentime and app/ytplayer [0.2.1]

--- app/screentime/screentime.js
+++ app/screentime/screentime.js
@@ -5,9 +5,13 @@
         let _sorted = Object.entries(KWS.screenTime).sort(([a1,a2],[b1,b2]) => b2-a2);
         let _rank = 1;
         for(let i of _sorted){
-            let _min = Math.floor(i[1]/60000);
+            let _hour = Math.floor(i[1]/3600000);
+            let _min = Math.floor(i[1]/60000%60);
             let _sec = Math.floor(i[1]/1000%60);
-            let _insert = `<kit-box><code class='m-r'>${_rank}</code><strong class='kit-left p-0'>${i[0]}</strong>${_min}分 ${_sec}秒</kit-box>`;
+            let _text = '';
+            if(_hour) _text = _hour + "時間 ";
+            _text += `${_min}分 ${_sec}秒`;
+            let _insert = `<kit-box><code class='m-r'>${_rank}</code><strong class='kit-left p-0'>${i[0]}</strong>${_text}</kit-box>`;
             _list.insertAdjacentHTML('beforeend', _insert);
             _rank ++;
         }
