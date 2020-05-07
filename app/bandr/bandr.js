005b37361936660d399ba9f871dcdca669b3f4b6
mtsgi <oq@live.jp>
2020-05-08 03:21:35 +0900

    Pass the object as an argument of data or event method [0.2.1]

--- app/bandr/bandr.js
+++ app/bandr/bandr.js
@@ -1,42 +1,46 @@
 ((_pid, _app) => {
-    _app.event('b', () => {
-        let _export = encodeURI(JSON.stringify(localStorage));
-        let _blob = new Blob([_export], { type: "text/plain" });
-        const a = document.createElement('a');
-        a.download = new Date().toISOString() + '.kitbackup';
-        a.href = URL.createObjectURL(_blob);
-        document.getElementById('bandr-download').href = URL.createObjectURL(_blob);
-        a.click();
-        a.parentNode.removeChild(a);
-    });
+  _app.event({
+    b() {
+      let _export = encodeURI(JSON.stringify(localStorage));
+      let _blob = new Blob([_export], { type: "text/plain" });
+      const a = document.createElement('a');
+      a.download = new Date().toISOString() + '.kitbackup';
+      a.href = URL.createObjectURL(_blob);
+      document.getElementById('bandr-download').href = URL.createObjectURL(_blob);
+      a.click();
+      a.parentNode.removeChild(a);
+    },
 
-    _app.event('r', (e) => {
-        const result = e.target.files[0];
-        const reader = new FileReader();
-        reader.readAsText(result);
-        reader.addEventListener('load', ()=>{
-            System.dialog("復元の確認", "本当に現在のプロファイルを破棄して復元を実行しますか？", () => {
-                System.dialog("復元を実行しますか？", _app.dom('#bandr-file').val() + "から現在のプロファイルが上書きされます(異なるプロファイルを復元しようとしている場合は必ず現在のプロファイルのバックアップファイルを保存してから復元を行ってください)。", () => {
-                    try {
-                        let _obj = JSON.parse( decodeURI( reader.result ) );
-                        let _list = new Array();
-                        for( let i in _obj ){
-                            if( i.indexOf("kit-") == 0 ){
-                                Notification.push("debug", i, "system");
-                                localStorage[i] = _obj[i];
-                                _list.push(i);
-                            }
-                        }
-                        _app.load('r3.html');
-                        System.alert( "復元の完了", "次の項目をkitシステムのプロファイルとして読み込みました:<hr>" + _list + "<hr>次の情報をシステムのプロファイルに再現しました:<hr>" + reader.result );
-                    } catch (error) {
-                        System.alert( "復元のエラー", `次のエラーにより、復元は完了しませんでした(以前のプロファイルが維持されています)。kitのバックアップファイル以外を選択している可能性があります。<blockquote>${error}</blockquote>` );
-                        return false;
-                    }
-                });
-            });
+    r(e) {
+      const result = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsText(result);
+      reader.addEventListener('load', () => {
+        System.dialog("復元の確認", "本当に現在のプロファイルを破棄して復元を実行しますか？", () => {
+          System.dialog("復元を実行しますか？", _app.dom('#bandr-file').val() + "から現在のプロファイルが上書きされます(異なるプロファイルを復元しようとしている場合は必ず現在のプロファイルのバックアップファイルを保存してから復元を行ってください)。", () => {
+            try {
+              let _obj = JSON.parse(decodeURI(reader.result));
+              let _list = new Array();
+              for (let i in _obj) {
+                if (i.indexOf("kit-") == 0) {
+                  Notification.push("debug", i, "system");
+                  localStorage[i] = _obj[i];
+                  _list.push(i);
+                }
+              }
+              _app.load('r3.html');
+              System.alert("復元の完了", "次の項目をkitシステムのプロファイルとして読み込みました:<hr>" + _list + "<hr>次の情報をシステムのプロファイルに再現しました:<hr>" + reader.result);
+            } catch (error) {
+              System.alert("復元のエラー", `次のエラーにより、復元は完了しませんでした(以前のプロファイルが維持されています)。kitのバックアップファイル以外を選択している可能性があります。<blockquote>${error}</blockquote>`);
+              return false;
+            }
+          });
         });
-    });
+      });
+    },
 
-    _app.event('reboot', ()=> System.reboot());
+    reboot: System.reboot
+  });
+
+  _app.event('d');
 })(pid, app);
--- app/devtools/devtools.js
+++ app/devtools/devtools.js
@@ -2,20 +2,24 @@
     _app.data('pid', pid);
     _app.event('load', () => {
         // Initialize
-        _app.data('launchpath', null);
-        _app.data('defobj', {});
-        _app.data('data', {});
-        _app.data('args', {});
+        _app.data({
+            launchpath: null,
+            defobj: {},
+            data: {},
+            args: {},
+            isopen: true
+        });
 
-        _app.data('isopen', true);
         let p = _app.data('pid');
         let lp = System.launchpath[p];
-        if(lp) {
-            _app.data('launchpath', lp);
-            _app.data('defobj', System.appCache[lp]);
-            _app.data('support', System.appCache[lp]['support']);
-            _app.data('data', App.data(p));
-            _app.data('args', System.args[p]);
+        if (lp) {
+            _app.data({
+                launchpath: lp,
+                defobj: System.appCache[lp],
+                support: System.appCache[lp]['support'],
+                data: App.data(p),
+                args: System.args[p]
+            });
         }
     });
     _app.event('set', () => {
