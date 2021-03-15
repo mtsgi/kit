0b4b92937dd8a2db949f54f5980cfd947ca887d9
mtsgi <oq@live.jp>
2021-03-16 02:03:33 +0900

    Format some system files [0.2.1]

--- service_worker.js
+++ service_worker.js
@@ -1,25 +1,25 @@
 var CACHE_NAME = 'kit-caches';
 var urlsToCache = [
-    '/mtsgi.github.io/',
-    '/exkit.netlify.app/'
+  '/mtsgi.github.io/',
+  '/exkit.netlify.app/'
 ];
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(function (response) {
+        return response ? response : fetch(event.request);
+      })
+  );
 });
